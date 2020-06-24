053cfbc00f60e8cf08f7c5e08c2aef70a39a4d62
范轶洁 <fanyjb@digitalchina.com>
2020-06-24 11:34:19 +0800

    with event

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,6 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 export async function mockFetchFoo() {
   return delay(2000).then(() => ({
     bar: "hello baz",
+    stamp: +Date.now(),
   }));
 }
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h2>{{ foo }}</h2>
+    <button @click="updateFoo">Update foo</button>
   </div>
 </template>
 
@@ -12,6 +13,11 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState({ foo: state => state.foo })
+  },
+  methods: {
+    updateFoo() {
+      this.$store.dispatch("updateFooData");
+    }
   },
   created() {
     this.$store.dispatch("updateFooData");
